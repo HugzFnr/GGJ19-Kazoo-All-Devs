f451476e80612a9c27b2533aabbafd970d32f6d2
Guillaume Grosse <ggrosse@ensc.fr>
2019-01-26 19:45:46 +0100

    Update salon.js

--- js/rooms/salon.js
+++ js/rooms/salon.js
@@ -2,12 +2,8 @@ salon = {};
 rooms.salon = salon;
 
 
-
-
-
 salon.x = (100 + 0)*zoomRooms;
 salon.y = (80 + 0)*zoomRooms;
-
 
 
 salon.width = 200*zoomRooms;
@@ -19,13 +15,27 @@ salon.begin = function()
 {
     var direction = 0;
 
+    //dimensions des vases
     var x = 0;
     var y = 0;
     var largeur = 50;
     var hauteur = 50;
 
+    //tableau des  vases
+    var tableau = [];
+
+    for(i = 0; i<15 ; i++)
+    {
+        var vase = {};
+        vase.x = rand(0,salon.width-largeur);
+        vase.y = 0;
+        vase.gravite = 0;
+        tableau.push(vase);
+    }
+
+    //position parent
     var largeur2 = 40;
-    var hauteur2 = 50;
+    var hauteur2 = 100;
     var x2 = (salon.width - largeur2) /2;
     var y2 = salon.height - hauteur2;
 
@@ -83,30 +93,60 @@ salon.begin = function()
 {
     context.clearRect(0,0, canvas.width, canvas.height);
     frame++;
-    
-    if (x < x2 + largeur2 &&
-        x + largeur > x2 &&
-        y < y2 + hauteur2 &&
-        y + hauteur > y2)
-        {
-            context.fillRect(x2, y2, largeur2, hauteur2);
-        }
-    else
+
+    for(j=0;j<15;j++)
     {
-        y++;
-        if(direction == -1 && x2 != 0)
+        if(frame == j*100)
         {
-            x2 += direction*2;
+            tableau[j].gravite = 1;
         }
 
-        if(direction == 1 && x2 != salon.width - largeur2)
+        //détection de collision
+        if (tableau[j].x < x2 + largeur2 &&
+            tableau[j].x + largeur > x2 &&
+            tableau[j].y < y2 + hauteur2 &&
+            tableau[j].y + hauteur > y2 &&
+            tableau[j].gravite == 1)
+            {
+                //tableau[j] = null; VOIR AVEC YOUNES
+                tableau[j].x = -largeur;
+                tableau[j].y = salon.height;
+                tableau[j].gravite = 0;
+            }            
+        else
+        if (tableau[j].gravite == 1)
         {
-            x2 += direction*2;
+            tableau[j].y+=2;
+            context.fillRect(tableau[j].x, tableau[j].y, largeur, hauteur);
+        }
+
+        if(tableau[j].y == salon.height - hauteur)
+        {
+            tableau[j].gravite = 0;
+            context.fillStyle = "red";
+            context.fillRect(tableau[j].x, tableau[j].y, largeur, hauteur);
+            context.fillStyle = "black";
         }
         
-        context.fillRect(x, y, largeur, hauteur);
-        context.fillRect(x2, y2, largeur2, hauteur2);
+        
+            
+        
+        
     }
+    
+
+    if(direction == -1 && x2 != 0)
+    {
+        x2 += direction*2;
+    }
+
+    if(direction == 1 && x2 != salon.width - largeur2)
+    {
+        x2 += direction*2;
+    }
+
+    context.fillRect(x2, y2, largeur2, hauteur2);
+    
         
 }, delta);
 }
