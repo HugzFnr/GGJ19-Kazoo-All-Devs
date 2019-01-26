8daa8afb4d9d8379db97dd24b9dc04191dfe7c85
Younès Rabii <rabiiyounes@outlook.com>
2019-01-26 11:18:50 +0100

    code de base

--- js/script.js
+++ js/script.js
@@ -1,58 +1,36 @@
-var container = document.getElementById('container');
-var network, nodes, edges;
+var canvas = document.getElementById("room");
+var context = canvas.getContext("2d");
+
+var bear = document.getElementById("sprite-bear");
 
 
-function init()
+var x = 20;
+var y = 10;
+var largeur = 100;
+var longeur = 69;
+
+
+function table(couleur, x, y)
 {
-	nodes = new vis.DataSet([createNode(0,0)]);
-	edges = new vis.DataSet([createEdge(0,0)]);
-	network = new vis.Network(container,{nodes:nodes,edges:edges},{});
+
+	context.fillStyle = couleur;
+	context.fillRect(x,y,largeur,longeur);
 }
 
 
-function createNode(id,label)
+function drawBear(x,y)
 {
-	return {id:id,label:label};
+	context.drawImage(bear, x ,y, bear.width/4, bear.height/4);
 }
 
-function createEdge(from,to)
+var delta = 1000/60; // 60 fps trop ouf
+
+var frame = 0;
+setInterval(function()
 {
-	return {from:from,to:to};
-}
+	context.clearRect(0,0, canvas.width, canvas.height);
+	frame++;
 
+	drawBear(0+frame,0+frame*2);
 
-  // var nodes = new vis.DataSet([
-        // {id: 1, label: 'Node 1'},
-        // {id: 2, label: 'Node 2'},
-        // {id: 3, label: 'Node 3'},
-        // {id: 4, label: 'Node 4'},
-        // {id: 5, label: 'Node 5'}
-    // ]);
-
-    // var edges = new vis.DataSet([
-        // {from: 1, to: 3},
-        // {from: 1, to: 2},
-        // {from: 2, to: 4},
-        // {from: 2, to: 5}
-    // ]);
-
-
-    // var data = {
-        // nodes: nodes,
-        // edges: edges
-    // };
-    // var options = {};
-
-   // initialize your network!
-// var network = new vis.Network(container, data, options);
-
-
-
-// var id = 0;
-// function addNode()
-// {
-	// nodes.add({id:1, label:"HEY"})
-// }
-
-
-init();
+}, delta);
