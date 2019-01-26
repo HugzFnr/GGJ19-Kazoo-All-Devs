9c1456284a1c565ffead7d4661ed0fe7ee63bb46
Hugo <hfournier001@ensc.fr>
2019-01-26 17:48:36 +0100

    WIP jeu couloir

--- js/params.js
+++ js/params.js
@@ -1,1 +1,2 @@
 var zoomRooms = 1.2;
+var deltaT = 1000/60;
--- js/rooms/couloir.js
+++ js/rooms/couloir.js
@@ -8,15 +8,50 @@ couloir.y = (80 + 150)*zoomRooms;
 couloir.width = 600*zoomRooms;
 couloir.height = 100*zoomRooms;
 
+var refx = couloir.x;
+var refy = couloir.y;
+var cw = couloir.width;
+var ch = couloir.height;
 
 //Le canvas
 couloir.cvs;
 
-
 couloir.begin = function()
 {
-  var contexte = couloir.cvs;
+  var ctx = couloir.cvs.getContext("2d");
 
+  var nbFails = 2;
+
+  var boxP = {
+    x : cw*(1/60),
+    y : ch*(1/120),
+    w : cw*(1/30),
+    h : ch*(1/2),
+}
+
+var boxL = {
+    x : cw*(19/20),
+    y : ch*(1/6),
+    w : cw*(1/30), 
+    h : ch*(1/5), 
+}
+
+  var speed = 5;
+  setInterval(function()
+  {
+    boxL.x-=speed;
+
+    ctx.clearRect(0,0,cw,ch);  
+
+    ctx.fillStyle= "green";
+    ctx.fillRect(boxP.x,boxP.y,boxP.w,boxP.h);
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(boxL.x,boxL.y,boxL.w,boxL.h);
+  }, deltaT);
+
+
+    
 
 
 }
